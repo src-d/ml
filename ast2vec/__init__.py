56169d6e073b0ab1c032bd6d818a9e3307928014
Vadim Markovtsev <vadim@sourced.tech>
2017-06-19 17:44:45 +0200

    Add ensure_bblfsh_is_running_noexc() to top level imports

--- ast2vec/__init__.py
+++ ast2vec/__init__.py
@@ -1,4 +1,4 @@
-from ast2vec.repo2base import Repo2Base
+from ast2vec.repo2base import Repo2Base, ensure_bblfsh_is_running_noexc
 from ast2vec.repo2coocc import Repo2Coocc, repo2coocc
 from ast2vec.repo2nbow import Repo2nBOW, repo2nbow
 from ast2vec.id2vec import Id2Vec
