bc567ef0a225e6e3982682166bee53b071e295fd
Vadim Markovtsev <vadim@sourced.tech>
2017-07-05 09:26:17 +0200

    Reorder imports

--- ast2vec/__init__.py
+++ ast2vec/__init__.py
@@ -1,10 +1,11 @@
+from ast2vec.__main__ import setup_logging
+from ast2vec.df import DocumentFrequencies
+from ast2vec.enry import install_enry
+from ast2vec.id2vec import Id2Vec
+from ast2vec.nbow import NBOW
 from ast2vec.repo2base import Repo2Base, ensure_bblfsh_is_running_noexc
 from ast2vec.repo2coocc import Repo2Coocc, repo2coocc
 from ast2vec.repo2nbow import Repo2nBOW, repo2nbow
-from ast2vec.id2vec import Id2Vec
-from ast2vec.df import DocumentFrequencies
-from ast2vec.nbow import NBOW
-from ast2vec.__main__ import setup_logging
-from ast2vec.enry import install_enry
+
 
 __version__ = 1, 0, 0
