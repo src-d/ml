f20bd2795a319c31cd3a4c6d6c59dd531187a824
Vadim Markovtsev <vadim@sourced.tech>
2017-07-06 19:01:50 +0200

    Add *_entry tests

--- ast2vec/tests/test_repo2nbow.py
+++ ast2vec/tests/test_repo2nbow.py
@@ -1,7 +1,12 @@
+import argparse
 import os
+import tempfile
 import unittest
 
+import asdf
+
 from ast2vec import repo2nbow, Id2Vec, DocumentFrequencies
+from ast2vec.repo2nbow import repo2nbow_entry
 import ast2vec.tests as tests
 from ast2vec.tests.models import ID2VEC, DOCFREQ
 
@@ -11,7 +16,7 @@ class Repo2NBOWTests(unittest.TestCase):
     def setUpClass(cls):
         tests.setup()
 
-    def test_ast2vec(self):
+    def test_obj(self):
         basedir = os.path.dirname(__file__)
         id2vec = Id2Vec(os.path.join(basedir, ID2VEC))
         df = DocumentFrequencies(os.path.join(basedir, DOCFREQ))
@@ -23,6 +28,20 @@ class Repo2NBOWTests(unittest.TestCase):
             id2vec=id2vec, df=df, linguist=tests.ENRY, timeout=600)
         self.assertIsInstance(nbow, dict)
         self.assertAlmostEqual(nbow[0], 13.79585695137316)
+
+    def test_asdf(self):
+        basedir = os.path.dirname(__file__)
+        with tempfile.NamedTemporaryFile() as file:
+            args = argparse.Namespace(
+                id2vec=os.path.join(basedir, ID2VEC),
+                df=os.path.join(basedir, DOCFREQ), linguist=tests.ENRY,
+                gcs=None, output=file.name, bblfsh=None, timeout=None,
+                repository=os.path.join(basedir, "..", ".."))
+            repo2nbow_entry(args)
+            data = asdf.open(file.name)
+            self.assertIn("meta", data.tree)
+            self.assertIn("nbow", data.tree)
+            self.assertEqual(2, len(data.tree["meta"]["dependencies"]))
 
 
 if __name__ == "__main__":
