3a222ea46fd04c6d786362ed2ddf558fe14b94c2
Timofei Semenov <timasem@gmail.com>
2017-08-07 12:18:09 +0000

    Add token parser

--- ast2vec/repo2/base.py
+++ ast2vec/repo2/base.py
@@ -30,18 +30,12 @@ class Repo2Base(PickleableLogger):
     `Babelfish <https://doc.bblf.sh/>`_ and source code identifier processing.
     """
     MODEL_CLASS = None  #: Must be defined in the children.
-    NAME_BREAKUP_RE = re.compile(r"[^a-zA-Z]+")  #: Regexp to split source code identifiers.
-    STEM_THRESHOLD = 6  #: We do not stem splitted parts shorter than or equal to this size.
-    MAX_TOKEN_LENGTH = 256  #: We cut identifiers longer than thi value.
     DEFAULT_BBLFSH_TIMEOUT = 10  #: Longer requests are dropped.
     MAX_FILE_SIZE = 200000
 
     def __init__(self, tempdir=None, linguist=None, log_level=logging.INFO,
                  bblfsh_endpoint=None, timeout=DEFAULT_BBLFSH_TIMEOUT):
         super(Repo2Base, self).__init__(log_level=log_level)
-        self._stemmer = Stemmer.Stemmer("english")
-        self._stemmer.maxCacheSize = 0
-        self._stem_threshold = self.STEM_THRESHOLD
         self._tempdir = tempdir
         self._cloner = RepoCloner(redownload=True, log_level=log_level)
         self._cloner.find_linguist(linguist)
@@ -159,52 +153,6 @@ class Repo2Base(PickleableLogger):
                   "and you hit https://github.com/bblfsh/server/issues/59#issuecomment-318125752"
             self._log.warning(msg)
             raise e from None
-
-    def _process_token(self, token):
-        for word in self._split(token):
-            yield self._stem(word)
-
-    def _stem(self, word):
-        if len(word) <= self._stem_threshold:
-            return word
-        return self._stemmer.stemWord(word)
-
-    @classmethod
-    def _split(cls, token):
-        token = token.strip()[:cls.MAX_TOKEN_LENGTH]
-        prev_p = [""]
-
-        def ret(name):
-            r = name.lower()
-            if len(name) >= 3:
-                yield r
-                if prev_p[0]:
-                    yield prev_p[0] + r
-                    prev_p[0] = ""
-            else:
-                prev_p[0] = r
-
-        for part in cls.NAME_BREAKUP_RE.split(token):
-            if not part:
-                continue
-            prev = part[0]
-            pos = 0
-            for i in range(1, len(part)):
-                this = part[i]
-                if prev.islower() and this.isupper():
-                    yield from ret(part[pos:i])
-                    pos = i
-                elif prev.isupper() and this.islower():
-                    if 0 < i - 1 - pos <= 3:
-                        yield from ret(part[pos:i - 1])
-                        pos = i - 1
-                    elif i - 1 > pos:
-                        yield from ret(part[pos:i])
-                        pos = i
-                prev = this
-            last = part[pos:]
-            if last:
-                yield from ret(last)
 
     def _get_log_name(self):
         return "repo2" + self.MODEL_CLASS.NAME
