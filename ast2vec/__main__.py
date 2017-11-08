618cd81b1b255c4ebd424023d661663beebb58a9
Vadim Markovtsev <vadim@sourced.tech>
2017-11-08 17:31:28 +0100

    Add some untested code for WMH
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- ast2vec/__main__.py
+++ ast2vec/__main__.py
@@ -1,6 +1,5 @@
 import argparse
 import logging
-import multiprocessing
 import os
 import sys
 
--- ast2vec/repo2/base.py
+++ ast2vec/repo2/base.py
@@ -1,9 +1,32 @@
 from ast2vec.pickleable_logger import PickleableLogger  # nopep8
 
 
-class Repo2Base(PickleableLogger):
-    def convert_uast(self, uast):
-        return self.convert_uasts([uast])
+class Repo2FinalizerBase(PickleableLogger):
+    def __call__(self, processed):
+        raise NotImplementedError()
 
-    def convert_uasts(self, file_uast_generator):
+
+class Repo2Base(PickleableLogger):
+    def __init__(self, engine, finalizer, languages=None, **kwargs):
+        super().__init__(**kwargs)
+        self.engine = engine
+        self.finalizer = finalizer
+        self.languages = languages or ("Java", "Python")
+
+    def _get_log_name(self):
+        return self.__class__.__name__
+
+    def process_files(self, files=None):
+        if files is None:
+            files = self.engine.repositories.references.head_ref.files
+        classified = files.classify_languages()
+        lang_filter = classified.lang == self.languages[0]
+        for lang in self.languages[1:]:
+            lang_filter |= classified.lang == lang
+        filtered_by_lang = classified.filter(lang_filter)
+        uasts = filtered_by_lang.extract_uasts()
+        processed = uasts.rdd.flatMap(self.process_uast)
+        self.finalizer(processed)
+
+    def process_uast(self, uast):
         raise NotImplementedError()
