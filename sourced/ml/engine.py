01fba889bf9c30674fcd0eb7713a1fbdc0228222
Vadim Markovtsev <vadim@sourced.tech>
2017-11-16 17:10:57 +0100

    Extract create_spark() and fix PYSPARK_PYTHON

--- sourced/ml/engine.py
+++ sourced/ml/engine.py
@@ -1,32 +1,32 @@
 import logging
 import os
 import sys
+os.environ["PYSPARK_PYTHON"] = sys.executable
 
 from pyspark.sql import SparkSession
 from sourced.engine import Engine
 
 
-__packages__ = [
-    "tech.sourced:engine:%s",
-    "com.datastax.spark:spark-cassandra-connector_2.11:2.0.3"
-]
-
-
-def create_engine(session_name, repositories, kwargs):
-    log = logging.getLogger("engine")
-    os.putenv("PYSPARK_PYTHON", sys.executable)
+def create_spark(session_name, kwargs):
+    log = logging.getLogger("spark")
     log.info("Starting %s on %s", session_name, kwargs.spark)
     builder = SparkSession.builder.master(kwargs.spark).appName(session_name)
     builder = builder.config(
-        "spark.jars.packages", ",".join(__packages__) % kwargs.engine)
-    builder = builder.config(
-        "spark.tech.sourced.bblfsh.grpc.host", kwargs.bblfsh)
-    # TODO(vmarkovtsev): figure out why is this option needed
-    builder = builder.config(
-        "spark.tech.sourced.engine.cleanup.skip", "true")
+        "spark.jars.packages", ",".join(kwargs.package))
     builder = builder.config("spark.local.dir", kwargs.spark_local_dir)
     for cfg in kwargs.config:
         builder = builder.config(*cfg.split("=", 1))
     session = builder.getOrCreate()
+    return session
+
+
+def create_engine(session_name, repositories, kwargs):
+    kwargs.config.append("spark.tech.sourced.bblfsh.grpc.host=" + kwargs.bblfsh)
+    # TODO(vmarkovtsev): figure out why is this option needed
+    kwargs.config.append("spark.tech.sourced.engine.cleanup.skip=true")
+    kwargs.package.append("tech.sourced:engine:" + kwargs.engine)
+    session = create_spark(session_name, kwargs)
+    log = logging.getLogger("engine")
+    log.info("Initializing on %s", repositories)
     engine = Engine(session, repositories)
     return engine
