7d6a52074c788bf709c15a08a8674ec8491adf2a
Vadim Markovtsev <vadim@sourced.tech>
2017-12-28 11:47:18 +0100

    Fix kwargs in Sequence extractors

--- sourced/ml/extractors/uast_seq.py
+++ sourced/ml/extractors/uast_seq.py
@@ -11,9 +11,12 @@ class UastSeqBagExtractor(BagsExtractor):
     OPTS.update(BagsExtractor.OPTS)
 
     def __init__(self, docfreq_threshold=None, **kwargs):
-        super().__init__(docfreq_threshold, **kwargs)
-        self._log.debug("__init__ %s", kwargs)
+        original_kwargs = kwargs
         uast2bag_kwargs = filter_kwargs(kwargs, UastSeq2Bag.__init__)
+        for k in uast2bag_kwargs:
+            kwargs.pop(k)
+        super().__init__(docfreq_threshold, **kwargs)
+        self._log.debug("__init__ %s", original_kwargs)
         self.uast2bag = UastSeq2Bag(**uast2bag_kwargs)
 
     def uast_to_bag(self, uast):
