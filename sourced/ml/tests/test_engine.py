f9e4dd6461ee047caf4fe888d71d674c5cc3e5fa
Romain Keramitas <r.keramitas@gmail.com>
2018-04-20 10:58:44 +0200

    Add test for engine and bblfsh dependency and modify default config for engine
    Signed-off-by: Romain Keramitas <r.keramitas@gmail.com>

---
+++ sourced/ml/tests/test_engine.py
@@ -0,0 +1,17 @@
+import unittest
+
+from sourced.ml.utils.engine import add_bblfsh_dependencies, add_engine_dependencies
+
+
+class EngineTests(unittest.TestCase):
+    def test_bblfsh_dependencies(self):
+        config = []
+        add_bblfsh_dependencies("localhost", config)
+        self.assertEqual(config, ["spark.tech.sourced.bblfsh.grpc.host=localhost"])
+
+    def test_engine_dependencies(self):
+        config = []
+        packages = []
+        add_engine_dependencies("latest", config, packages)
+        self.assertEqual(config, ["spark.tech.sourced.engine.cleanup.skip=false"])
+        self.assertEqual(packages, ["tech.sourced:engine:latest"])
