31c210687fbb61495c03ac08765d43d3fe6478b9
egor <egor@sourced.tech>
2018-05-22 13:09:35 +0200

    Tests for basic transformers * ParquetSaver * ParquetLoader * Collector * First * Identity * FieldsSelector
    Signed-off-by: egor <egor@sourced.tech>

--- sourced/ml/tests/test_basic_transformers.py
+++ sourced/ml/tests/test_basic_transformers.py
@@ -1,7 +1,12 @@
+import shutil
+import tempfile
 import unittest
 
+from pyspark.sql import Row
+
 from sourced.ml.utils.spark import create_spark
-from sourced.ml.transformers.basic import ParquetLoader, Repartitioner
+from sourced.ml.transformers.basic import ParquetSaver, ParquetLoader, Collector, First, \
+     Identity, FieldsSelector, Repartitioner
 from sourced.ml.tests.models import PARQUET_DIR
 
 
@@ -20,6 +25,62 @@ class BasicTransformerTest(unittest.TestCase):
 
         repartitioned_data = Repartitioner(partitions, shuffle=True)(data)
         self.assertEqual(partitions, repartitioned_data.getNumPartitions())
+
+    def test_parquet_loader(self):
+        # load parquet and check number of rows
+        loader = ParquetLoader(session=self.spark, paths=PARQUET_DIR)
+        data = loader.execute()
+        self.assertEqual(data.count(), 6)
+
+    def test_identity(self):
+        # load parquet
+        loader = ParquetLoader(session=self.spark, paths=PARQUET_DIR)
+        data = loader.execute()
+        # check that identity returns the same RDD
+        data_identity = Identity()(data)
+        self.assertEqual(data_identity.count(), 6)
+        self.assertEqual(data_identity, data)
+
+    def test_parquet_saver(self):
+        with tempfile.TemporaryDirectory() as tmpdir:
+            dirname = tmpdir
+        try:
+            # load and save data
+            rows = [("Alice", 1)]
+            df = self.spark.createDataFrame(rows, ["name", "age"])
+            ParquetSaver(dirname + "/")(df.rdd)
+
+            # read saved data and check it
+            data = ParquetLoader(session=self.spark, paths=dirname).execute()
+            self.assertEqual(data.count(), 1)
+        finally:
+            shutil.rmtree(dirname)
+
+    def test_collector(self):
+        data = ParquetLoader(session=self.spark, paths=PARQUET_DIR).link(Collector()) \
+            .execute()
+        self.assertEqual(len(data), 6)
+
+    def test_first(self):
+        row = ParquetLoader(session=self.spark, paths=PARQUET_DIR).link(First()) \
+            .execute()
+        self.assertTrue(isinstance(row, Row))
+
+    def test_field_selector(self):
+        rows = [("Alice", 1)]
+        df = self.spark.createDataFrame(rows, ["name", "age"])
+        # select field "name"
+        row = FieldsSelector(fields=["name"])(df.rdd).first()
+        self.assertFalse(hasattr(row, "age"))
+        self.assertTrue(hasattr(row, "name"))
+        # select field "age"
+        row = FieldsSelector(fields=["age"])(df.rdd).first()
+        self.assertTrue(hasattr(row, "age"))
+        self.assertFalse(hasattr(row, "name"))
+        # select field "name" and "age"
+        row = FieldsSelector(fields=["name", "age"])(df.rdd).first()
+        self.assertTrue(hasattr(row, "age"))
+        self.assertTrue(hasattr(row, "name"))
 
 
 if __name__ == '__main__':
