0ff98e181e2b23fb64e8ff8986c576b8a0ac80cb
Vadim Markovtsev <vadim@sourced.tech>
2018-08-30 17:04:48 +0200

    Add the missing modelforge configuration
    Signed-off-by: Vadim Markovtsev <vadim@sourced.tech>

--- sourced/ml/modelforgecfg.py
+++ sourced/ml/modelforgecfg.py
@@ -1,3 +1,8 @@
+import os
+
+
 VENDOR = "source{d}"
 BACKEND = "gcs"
 BACKEND_ARGS = "bucket=models.cdn.sourced.tech"
+INDEX_REPO = "https://github.com/src-d/models"
+CACHE_DIR = os.path.join(os.path.expanduser("~"), ".cache", "source{d}")
