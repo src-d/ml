21b401076167e7b37b5d164622cc2ccbf607f057
egor <egor@sourced.tech>
2019-02-12 14:50:47 +0100

    Update requirements and rename `engine` to `jgit-spark-connector`
    Signed-off-by: egor <egor@sourced.tech>

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "PyStemmer>=1.3,<2.0",
         "bblfsh>=2.2.1,<3.0",
         "modelforge>=0.11.0,<0.12",
-        "sourced-engine>=0.7.0,<1.1",
+        "sourced-jgit-spark-connector>=2.0.1,<2.1.0",
         "humanize>=0.5.0,<0.6",
         "parquet>=1.2,<2.0",
         "pygments>=2.2.0,<3.0",
